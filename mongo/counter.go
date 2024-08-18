9a47c4ed3cc697bd250dda98e3a84d344d2deea2
Satoshi Konno <skonno@cybergarage.org>
2024-08-18 20:22:53 +0900

    Add Counter

---
+++ mongo/counter.go
@@ -0,0 +1,36 @@
+// Copyright (C) 2019 The go-mongo Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mongo
+
+// Counter is a counter.
+type Counter struct {
+	count uint32
+}
+
+// NewCounter returns a new counter.
+func NewCounter() *Counter {
+	return &Counter{
+		count: 0,
+	}
+}
+
+// Inc increments the counter and returns the new value.
+func (counter *Counter) Inc() uint32 {
+	if counter.count == ^uint32(0) {
+		counter.count = 0
+	}
+	counter.count++
+	return counter.count
+}
