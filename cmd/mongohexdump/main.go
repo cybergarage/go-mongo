bf2e0c6b88c1f92ee8f7be38f09f3f549b00b94d
Satoshi Konno <skonno@cybergarage.org>
2024-08-21 23:01:35 +0900

    Add mongohexdump

---
+++ cmd/mongohexdump/main.go
@@ -0,0 +1,58 @@
+// Copyright (C) 2019 The go-mongo Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+monghexdump is a dump utility for MongoDB packet hexdump file.
+
+	NAME
+	 monghexdump
+
+	SYNOPSIS
+	 monghexdump FILE
+
+	RETURN VALUE
+	  Return EXIT_SUCCESS or EXIT_FAILURE
+*/
+//nolint:forbidigo
+package main
+
+import (
+	"flag"
+	"os"
+)
+
+const (
+	ProgramName = "monghexdump"
+)
+
+func usages() {
+	println("Usage:")
+	println("  " + ProgramName + " [OPTIONS] FILE")
+	println("")
+	println("Return Value:")
+	println("  Return EXIT_SUCCESS or EXIT_FAILURE")
+	os.Exit(1)
+}
+
+func main() {
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		usages()
+	}
+
+	// pcapFilename := args[0]
+	os.Exit(0)
+}
